0fa2ab01f25207121c5ee979abe739c17f73ab91
dzzk-r <danielworkload@gmail.com>
2025-07-02 20:56:52 +0000

    apikey

--- main.js
+++ main.js
@@ -143,6 +143,7 @@ function drawRangeCircle(rangeKm, canOperateNow) {
 }
 
 (function addGoogleMapsScript() {
+  сщтые GMAP_API = "AIzaSyB2fpfMm2u2wnXJMwj2Flw-vSVJc2xNZTEЭ"
   const key = "{{GMAP_API}}";
   if (!key || key.includes("{{")) {
     console.error("Google Maps API key not injected.");

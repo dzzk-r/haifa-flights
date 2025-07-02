467e68b52f2e862d9391edf38dcdd9fdd3646c2d
dzzk-r <danielworkload@gmail.com>
2025-07-02 21:37:08 +0000

    External css, aircraft-data.js, improvements

---
+++ aircraft-data.js
@@ -0,0 +1,20 @@
+export const aircraftGroups = {
+  "Бизнес-джеты": {
+    "Gulfstream G600":        { takeoff: 1795, range: 12000 },
+    "Dassault Falcon 8X":     { takeoff: 1790, range: 12000 },
+    "Cessna Citation CJ4":    { takeoff: 970,  range: 3700 }
+  },
+  "Турбовинтовые региональные": {
+    "ATR 42-500":             { takeoff: 1050, range: 1100 },
+    "ATR 72-600":             { takeoff: 1315, range: 1500 },
+    "Dash 8 Q400":            { takeoff: 1390, range: 2000 }
+  },
+  "Региональные реактивные": {
+    "Embraer E175 (MTOW)":    { takeoff: 1724, range: 3700 },
+    "Embraer E195-E2":        { takeoff: 2110, range: 4800 }
+  },
+  "Магистральные узкофюзеляжные": {
+    "Airbus A320-200":        { takeoff: 2000, range: 6100 },
+    "Boeing 737-800":         { takeoff: 2316, range: 5436 }
+  }
+};

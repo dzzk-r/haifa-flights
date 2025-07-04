076085a3243d4648715c22c28f1fd30ae8aa4322
dzzk-r <danielworkload@gmail.com>
2025-07-04 06:58:11 +0000

    runway update

--- runway.js
+++ runway.js
@@ -1,20 +1,27 @@
-export function drawRunway(map) {
-  const runwayBounds = {
-    north: 32.8185,
-    south: 32.8105,
-    east: 35.0505,
-    west: 35.0395,
-  };
+let runwayPolygon;
 
-  const runwayRect = new google.maps.Rectangle({
-    strokeColor: '#000000',
+export function drawRunway(map) {
+  const runwayCoords = [
+    { lat: 32.8157816, lng: 35.0411338 },
+    { lat: 32.8046002, lng: 35.0460476 },
+    { lat: 32.8046994, lng: 35.0463587 },
+    { lat: 32.8158898, lng: 35.0414342 },
+    { lat: 32.8157816, lng: 35.0411338 } // закрытие контура
+  ];
+
+  if (runwayPolygon) {
+    runwayPolygon.setMap(null); // удалить старый, если есть
+  }
+
+  runwayPolygon = new google.maps.Polygon({
+    paths: runwayCoords,
+    strokeColor: "#ff0000",
     strokeOpacity: 0.8,
     strokeWeight: 2,
-    fillColor: '#555555',
-    fillOpacity: 0.4,
-    map,
-    bounds: runwayBounds,
+    fillColor: "#ff0000",
+    fillOpacity: 0.2,
+    clickable: false
   });
 
-  return runwayRect;
+  runwayPolygon.setMap(map);
 }
